4aee8d45586772a93b2e1254236ab0a48a4381b4
LuckiMoH <mat.maj1992@gmail.com>
2022-11-16 13:58:55 +0100

    Add job page finished

--- src/components/FormRow.js
+++ src/components/FormRow.js
@@ -4,7 +4,7 @@ const FormRow = ({type, name, value, handleChange, labelText}) => {
     return (
         <div className="form-row">
             <label htmlFor={name} className='form-label'>{labelText || name}</label>
-            <input type={type} name={name} value={value} onChange={handleChange} className='form-input'/>
+            <input id={name} type={type} name={name} value={value} onChange={handleChange} className='form-input'/>
         </div>
     );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ import FormRow from './FormRow';
 import Navbar from "./Navbar";
 import BigSidebar from "./BigSidebar";
 import SmallSidebar from "./SmallSidebar";
+import FormRowSelect from "./FormRowSelect";
 
 
-export { Logo, NotFound, FormRow, Navbar, SmallSidebar, BigSidebar };
+export { Logo, NotFound, FormRow, Navbar, SmallSidebar, BigSidebar, FormRowSelect };
